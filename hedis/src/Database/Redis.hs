fd169b8c32cfcf099fd40b45b7b84e2f4ae41337
Vi Po <pi@homedir.eu>
2015-10-27 09:20:53 +0200

    Patch hedis

---
+++ hedis/src/Database/Redis.hs
@@ -0,0 +1,172 @@
+module Database.Redis (
+    -- * How To Use This Module
+    -- |
+    -- Connect to a Redis server:
+    --
+    -- @
+    -- -- connects to localhost:6379
+    -- conn <- 'connect' 'defaultConnectInfo'
+    -- @
+    --
+    -- Send commands to the server:
+    -- 
+    -- @
+    -- {-\# LANGUAGE OverloadedStrings \#-}
+    -- ...
+    -- 'runRedis' conn $ do
+    --      'set' \"hello\" \"hello\"
+    --      set \"world\" \"world\"
+    --      hello <- 'get' \"hello\"
+    --      world <- get \"world\"
+    --      liftIO $ print (hello,world)
+    -- @
+
+    -- ** Command Type Signatures
+    -- |Redis commands behave differently when issued in- or outside of a
+    --  transaction. To make them work in both contexts, most command functions
+    --  have a type signature similar to the following:
+    --
+    --  @
+    --  'echo' :: ('RedisCtx' m f) => ByteString -> m (f ByteString)
+    --  @
+    --
+    --  Here is how to interpret this type signature:
+    --
+    --  * The argument types are independent of the execution context. 'echo'
+    --    always takes a 'ByteString' parameter, whether in- or outside of a
+    --    transaction. This is true for all command functions.
+    --
+    --  * All Redis commands return their result wrapped in some \"container\".
+    --    The type @f@ of this container depends on the commands execution
+    --    context @m@. The 'ByteString' return type in the example is specific
+    --    to the 'echo' command. For other commands, it will often be another
+    --    type.
+    --
+    --  * In the \"normal\" context 'Redis', outside of any transactions,
+    --    results are wrapped in an @'Either' 'Reply'@.
+    --
+    --  * Inside a transaction, in the 'RedisTx' context, results are wrapped in
+    --    a 'Queued'.
+    --
+    --  In short, you can view any command with a 'RedisCtx' constraint in the
+    --  type signature, to \"have two types\". For example 'echo' \"has both
+    --  types\":
+    --
+    --  @
+    --  echo :: ByteString -> Redis (Either Reply ByteString)
+    --  echo :: ByteString -> RedisTx (Queued ByteString)
+    --  @
+    --
+    --  [Exercise] What are the types of 'expire' inside a transaction and
+    --    'lindex' outside of a transaction? The solutions are at the very
+    --    bottom of this page.
+
+    -- ** Lua Scripting
+    -- |Lua values returned from the 'eval' and 'evalsha' functions will be
+    --  converted to Haskell values by the 'decode' function from the
+    --  'RedisResult' type class.
+    --
+    --  @
+    --  Lua Type      | Haskell Type       | Conversion Example
+    --  --------------|--------------------|-----------------------------
+    --  Number        | Integer            | 1.23   => 1
+    --  String        | ByteString, Double | \"1.23\" => \"1.23\" or 1.23
+    --  Boolean       | Bool               | false  => False
+    --  Table         | List               | {1,2}  => [1,2]
+    --  @
+    --
+    --  Additionally, any of the Haskell types from the table above can be
+    --  wrapped in a 'Maybe':
+    --
+    --  @
+    --  42  => Just 42 :: Maybe Integer
+    --  nil => Nothing :: Maybe Integer
+    --  @
+    --
+    --  Note that Redis imposes some limitations on the possible conversions:
+    --
+    --  * Lua numbers can only be converted to Integers. Only Lua strings can be
+    --    interpreted as Doubles.
+    --
+    --  * Associative Lua tables can not be converted at all. Returned tables
+    --   must be \"arrays\", i.e. indexed only by integers.
+    --
+    --  The Redis Scripting website (<http://redis.io/commands/eval>)
+    --  documents the exact semantics of the scripting commands and value
+    --  conversion.
+    
+    -- ** Automatic Pipelining
+    -- |Commands are automatically pipelined as much as possible. For example,
+    --  in the above \"hello world\" example, all four commands are pipelined.
+    --  Automatic pipelining makes use of Haskell's laziness. As long as a
+    --  previous reply is not evaluated, subsequent commands can be pipelined.
+    --
+    --  Automatic pipelining also works across several calls to 'runRedis', as
+    --  long as replies are only evaluated /outside/ the 'runRedis' block.
+    --
+    --  To keep memory usage low, the number of requests \"in the pipeline\" is
+    --  limited (per connection) to 1000. After that number, the next command is
+    --  sent only when at least one reply has been received. That means, command
+    --  functions may block until there are less than 1000 outstanding replies.
+    --
+    
+    -- ** Error Behavior
+    -- |
+    --  [Operations against keys holding the wrong kind of value:] Outside of a
+    --    transaction, if the Redis server returns an 'Error', command functions
+    --    will return 'Left' the 'Reply'. The library user can inspect the error
+    --    message to gain  information on what kind of error occured.
+    --
+    --  [Connection to the server lost:] In case of a lost connection, command
+    --    functions throw a 'ConnectionLostException'. It can only be caught
+    --    outside of 'runRedis'.
+    --
+    --  [Exceptions:] Any exceptions can only be caught /outside/ of 'runRedis'.
+    --    This way the connection pool can properly close the connection, making
+    --    sure it is not left in an unusable state, e.g. closed or inside a
+    --    transaction.
+    --
+    
+    -- * The Redis Monad
+    Redis(), runRedis,
+    RedisCtx(), MonadRedis(),
+
+    -- * Connection
+    Connection, connect,
+    ConnectInfo(..),defaultConnectInfo,
+    HostName,PortID(..),
+    
+    -- * Commands
+    module Database.Redis.Commands,
+    
+    -- * Transactions
+    module Database.Redis.Transactions,
+    
+    -- * Pub\/Sub
+    module Database.Redis.PubSub,
+
+    -- * Low-Level Command API
+    sendRequest,
+    Reply(..),Status(..),RedisResult(..),ConnectionLostException(..),
+    
+    -- |[Solution to Exercise]
+    --
+    --  Type of 'expire' inside a transaction:
+    --
+    --  > expire :: ByteString -> Integer -> RedisTx (Queued Bool)
+    --
+    --  Type of 'lindex' outside of a transaction:
+    --
+    --  > lindex :: ByteString -> Integer -> Redis (Either Reply ByteString)
+    --
+) where
+
+import Database.Redis.Core
+import Database.Redis.PubSub
+import Database.Redis.Protocol
+import Database.Redis.ProtocolPipelining
+    (HostName, PortID(..), ConnectionLostException(..))
+import Database.Redis.Transactions
+import Database.Redis.Types
+
+import Database.Redis.Commands
---
+++ hedis/src/Database/Redis/Core.hs
@@ -0,0 +1,187 @@
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving, RecordWildCards,
+    MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
+
+module Database.Redis.Core (
+    Connection, connect,
+    ConnectInfo(..), defaultConnectInfo,
+    Redis(),runRedis,
+    RedisCtx(..), MonadRedis(..),
+    send, recv, sendRequest,
+    auth, select
+) where
+
+import Prelude
+import Control.Applicative
+import Control.Monad.Reader
+import qualified Data.ByteString as B
+import Data.Pool
+import Data.Time
+import Network
+
+import Database.Redis.Protocol
+import qualified Database.Redis.ProtocolPipelining as PP
+import Database.Redis.Types
+
+
+--------------------------------------------------------------------------------
+-- The Redis Monad
+--
+
+-- |Context for normal command execution, outside of transactions. Use
+--  'runRedis' to run actions of this type.
+--
+--  In this context, each result is wrapped in an 'Either' to account for the
+--  possibility of Redis returning an 'Error' reply.
+newtype Redis a = Redis (ReaderT (PP.Connection Reply) IO a)
+    deriving (Monad, MonadIO, Functor, Applicative)
+
+-- |This class captures the following behaviour: In a context @m@, a command
+--  will return it's result wrapped in a \"container\" of type @f@.
+--
+--  Please refer to the Command Type Signatures section of this page for more
+--  information.
+class (MonadRedis m) => RedisCtx m f | m -> f where
+    returnDecode :: RedisResult a => Reply -> m (f a)
+
+instance RedisCtx Redis (Either Reply) where
+    returnDecode = return . decode
+
+class (Monad m) => MonadRedis m where
+    liftRedis :: Redis a -> m a
+
+instance MonadRedis Redis where
+    liftRedis = id
+
+-- |Interact with a Redis datastore specified by the given 'Connection'.
+--
+--  Each call of 'runRedis' takes a network connection from the 'Connection'
+--  pool and runs the given 'Redis' action. Calls to 'runRedis' may thus block
+--  while all connections from the pool are in use.
+runRedis :: Connection -> Redis a -> IO a
+runRedis (Conn pool) redis =
+    withResource pool $ \conn -> runRedisInternal conn redis
+
+-- |Internal version of 'runRedis' that does not depend on the 'Connection'
+--  abstraction. Used to run the AUTH command when connecting. 
+runRedisInternal :: PP.Connection Reply -> Redis a -> IO a
+runRedisInternal env (Redis redis) = runReaderT redis env
+
+
+recv :: (MonadRedis m) => m Reply
+recv = liftRedis $ Redis $ ask >>= liftIO . PP.recv
+
+send :: (MonadRedis m) => [B.ByteString] -> m ()
+send req = liftRedis $ Redis $ do
+    conn <- ask
+    liftIO $ PP.send conn (renderRequest req)
+
+-- |'sendRequest' can be used to implement commands from experimental
+--  versions of Redis. An example of how to implement a command is given
+--  below.
+--
+-- @
+-- -- |Redis DEBUG OBJECT command
+-- debugObject :: ByteString -> 'Redis' (Either 'Reply' ByteString)
+-- debugObject key = 'sendRequest' [\"DEBUG\", \"OBJECT\", key]
+-- @
+--
+sendRequest :: (RedisCtx m f, RedisResult a)
+    => [B.ByteString] -> m (f a)
+sendRequest req = do
+    r <- liftRedis $ Redis $ do
+        conn <- ask
+        liftIO $ PP.request conn (renderRequest req)
+    returnDecode r
+
+
+--------------------------------------------------------------------------------
+-- Connection
+--
+
+-- |A threadsafe pool of network connections to a Redis server. Use the
+--  'connect' function to create one.
+newtype Connection = Conn (Pool (PP.Connection Reply))
+
+-- |Information for connnecting to a Redis server.
+--
+-- It is recommended to not use the 'ConnInfo' data constructor directly.
+-- Instead use 'defaultConnectInfo' and update it with record syntax. For
+-- example to connect to a password protected Redis server running on localhost
+-- and listening to the default port:
+-- 
+-- @
+-- myConnectInfo :: ConnectInfo
+-- myConnectInfo = defaultConnectInfo {connectAuth = Just \"secret\"}
+-- @
+--
+data ConnectInfo = ConnInfo
+    { connectHost           :: HostName
+    , connectPort           :: PortID
+    , connectAuth           :: Maybe B.ByteString
+    -- ^ When the server is protected by a password, set 'connectAuth' to 'Just'
+    --   the password. Each connection will then authenticate by the 'auth'
+    --   command.
+    , connectDatabase       :: Integer
+    -- ^ Each connection will 'select' the database with the given index.
+    , connectMaxConnections :: Int
+    -- ^ Maximum number of connections to keep open. The smallest acceptable
+    --   value is 1.
+    , connectMaxIdleTime    :: NominalDiffTime
+    -- ^ Amount of time for which an unused connection is kept open. The
+    --   smallest acceptable value is 0.5 seconds. If the @timeout@ value in
+    --   your redis.conf file is non-zero, it should be larger than
+    --   'connectMaxIdleTime'.
+    } deriving Show
+
+-- |Default information for connecting:
+--
+-- @
+--  connectHost           = \"localhost\"
+--  connectPort           = PortNumber 6379 -- Redis default port
+--  connectAuth           = Nothing         -- No password
+--  connectDatabase       = 0               -- SELECT database 0
+--  connectMaxConnections = 50              -- Up to 50 connections
+--  connectMaxIdleTime    = 30              -- Keep open for 30 seconds
+-- @
+--
+defaultConnectInfo :: ConnectInfo
+defaultConnectInfo = ConnInfo
+    { connectHost           = "localhost"
+    , connectPort           = PortNumber 6379
+    , connectAuth           = Nothing
+    , connectDatabase       = 0
+    , connectMaxConnections = 50
+    , connectMaxIdleTime    = 30
+    }
+
+-- |Opens a 'Connection' to a Redis server designated by the given
+--  'ConnectInfo'.
+connect :: ConnectInfo -> IO Connection
+connect ConnInfo{..} = Conn <$>
+    createPool create destroy 40 connectMaxIdleTime connectMaxConnections
+  where
+    create = do
+        conn <- PP.connect connectHost connectPort reply
+        runRedisInternal conn $ do
+            -- AUTH
+            case connectAuth of
+                Nothing   -> return ()
+                Just pass -> void $ auth pass
+            -- SELECT
+            when (connectDatabase /= 0) (void $ select connectDatabase)
+        return conn
+
+    destroy = PP.disconnect
+
+-- The AUTH command. It has to be here because it is used in 'connect'.
+auth
+    :: B.ByteString -- ^ password
+    -> Redis (Either Reply Status)
+auth password = sendRequest ["AUTH", password]
+
+-- The SELECT command. Used in 'connect'.
+select
+    :: RedisCtx m f
+    => Integer -- ^ index
+    -> m (f Status)
+select ix = sendRequest ["SELECT", encode ix]
